def585c23d42b7188177e1e3da73e53131dd9e9f
OakAndAspen <irinadespot@gmail.com>
2018-06-02 19:28:14 +0200

    Worked on admin intranet

---
+++ src/views/JuniorsList.js
@@ -0,0 +1,17 @@
+import Backbone from "backbone";
+import template from "templates/pages/admin/junior/index.handlebars";
+
+export default Backbone.View.extend({
+
+    initialize: function (attrs, options) {
+        this.template = template;
+        this.listenTo(this.collection, "change add remove", this.render);
+    },
+
+    render: function () {
+        this.$el.html(this.template({
+            juniors: this.collection.toJSON()
+        }));
+        return this.$el;
+    }
+});
