4c5bc14619568674200f85f52912cfa63685eb6f
juanmorenoHEIG <juan.morenoizquierdo@heig-vd.ch>
2018-06-06 15:28:08 +0200

    frewfrewf

---
+++ src/models/Suggestion.js
@@ -0,0 +1,13 @@
+import Backbone from "backbone";
+
+export default Backbone.Model.extend({
+
+    initialize: function (attrs, options) {
+
+    },
+
+    validate: function (attrs, options) {
+
+    }
+
+});
---
+++ src/views/special/ProfilJunior.js
@@ -0,0 +1,16 @@
+import Backbone from "backbone";
+import template from "templates/special/ProfilJunior.handlebars";
+
+export default Backbone.View.extend({
+
+    initialize: function (attrs, options) {
+        this.template = template;
+        this.listenTo(this.model, "change add remove", this.render);
+    },
+
+    render: function () {
+        this.$el.html(this.template(this.model.toJSON()));
+
+        return this.$el;
+    }
+});
