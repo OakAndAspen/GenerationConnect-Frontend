a2cb47d8ddb7a0b617dbb0fa22579f4e41c6ddee
juanmorenoHEIG <juan.morenoizquierdo@heig-vd.ch>
2018-06-14 10:23:54 +0200

    Merge branch 'master' of https://github.com/OakAndAspen/GenerationConnect-Frontend
    # Conflicts:
    #	src/routers/JuniorRouter.js
    #	src/views/lists/ListInterventionsJunior.js

--- src/views/forms/FormPostulation.js
+++ src/views/forms/FormPostulation.js
@@ -14,21 +14,24 @@ export default Backbone.View.extend({
     },
 
     events: {
-        'click #send': 'send'
+        'submit form': 'submit'
     },
 
-    send: function (event) {
+    submit: function (event) {
+
+        event.preventDefault();
+
         let email = $("#email").val();
         let prenom = $("#prenom").val();
         let nom = $("#nom").val();
-        let motdepasse = "";
-        if($("#motdepasse1").val() == $("#motdepasse2").val()){
-            motdepasse = $("#motdepasse1").val();
-        };
+        let motdepasse = $("#motdepasse1").val();
+        let motdepasse2 = $("#motdepasse2").val();
+
+
         let phone = $("#telephone").val();
         let limitetemps = $("#limitetemps").val();
 
-        let ligne1hab = $("#ligne1han").val();
+        let ligne1hab = $("#ligne1hab").val();
         let ligne2hab = $("#ligne2hab").val();
         let ligne3hab = $("#ligne3hab").val();
         let NoNPAhab = $("#noNPAhab").val();
@@ -39,57 +42,113 @@ export default Backbone.View.extend({
         let banquebic = $("#banquebic").val();
         let banqueiban = $("#banqueiban").val();
 
+        let ligne1loc = $("#ligne1hab").val();
+        let ligne2loc = $("#ligne2hab").val();
+        let ligne3loc = $("#ligne3hab").val();
+        let NoNPAloc = $("#noNPAhab").val();
+        let villeloc = $("#villehab").val();
+
         if($("#adresse1").val() != 0){
-            let ligne1loc = $("#ligne1loc").val();
-            let ligne2loc = $("#ligne2loc").val();
-            let ligne3loc = $("#ligne3loc").val();
-            let NoNPAloc = $("#noNPAloc").val();
-            let villeloc = $("#villeloc").val();
+            ligne1loc = $("#ligne1loc").val();
+            ligne2loc = $("#ligne2loc").val();
+            ligne3loc = $("#ligne3loc").val();
+            NoNPAloc = $("#noNPAloc").val();
+            villeloc = $("#villeloc").val();
         }
+
+        let ligne1fac = $("#ligne1hab").val();
+        let ligne2fac = $("#ligne2hab").val();
+        let ligne3fac = $("#ligne3hab").val();
+        let NoNPAfac = $("#noNPAhab").val();
+        let villefac = $("#villehab").val();
 
         if($("#adresse2").val() != 0){
-            let ligne1fac = $("#ligne1fac").val();
-            let ligne2fac = $("#ligne2fac").val();
-            let ligne3fac = $("#ligne3fac").val();
-            let NoNPAfac = $("#noNPAfac").val();
-            let villefac = $("#villefac").val();
+            ligne1fac = $("#ligne1fac").val();
+            ligne2fac = $("#ligne2fac").val();
+            ligne3fac = $("#ligne3fac").val();
+            NoNPAfac = $("#noNPAfac").val();
+            villefac = $("#villefac").val();
         }
 
-        let fileCV = $("#cvFile").files;
+
+
+        //let fileCV = $("#cvFile").prop('files')[0];
+
+        // let formData = new FormData($("#formPostulation")[0]);
+
+        let form = new FormData();
+
+
+        form.append("prenom",prenom);
+        form.append("nom",nom);
+        form.append("email",email);
+        form.append("motdepasse",motdepasse);
+        form.append("motdepasse_confirmation",motdepasse2);
+        form.append("telephone",phone);
+
+        form.append("adresse_habitation[ligne1]",ligne1hab);
+        form.append("adresse_habitation[ligne2]",ligne2hab);
+        form.append("adresse_habitation[ligne3]",ligne3hab);
+        form.append("adresse_habitation[ville]",villehab);
+        form.append("adresse_habitation[npa]",NoNPAhab);
+
+        form.append("adresse_depart[ligne1]",ligne1loc);
+        form.append("adresse_depart[ligne2]",ligne2loc);
+        form.append("adresse_depart[ligne3]",ligne3loc);
+        form.append("adresse_depart[ville]",villeloc);
+        form.append("adresse_depart[npa]",NoNPAloc);
+
+        form.append("adresse_facturation[ligne1]",ligne1fac);
+        form.append("adresse_facturation[ligne2]",ligne2fac);
+        form.append("adresse_facturation[ligne3]",ligne3fac);
+        form.append("adresse_facturation[ville]",villefac);
+        form.append("adresse_facturation[npa]",NoNPAfac);
+
+        form.append("NoAVS",avs);
+        form.append("BanqueNom",banquenom);
+        form.append("BanqueBIC",banquebic);
+        form.append("BanqueIBAN",banqueiban);
+        form.append("LimiteTempsTransport",limitetemps);
+
+        form.append("cv", $("#cvFile")[0].files[0]);
 
         $.ajax({
             type: "POST",
             url: AppConfig.apiUrl + "/inscription/junior",
-            data: {
+            cache: false,
+            processData: false,
+            contentType: false,
+            data: form,
+            /*data: {
                 prenom: prenom,
                 nom: nom,
                 email: email,
-                content: message,
                 motdepasse: motdepasse,
+                motdepasse_confirmation: motdepasse2,
                 telephone: phone,
-                Status: "candidat",
                 'adresse_habitation[ligne1]': ligne1hab,
                 'adresse_habitation[ligne2]': ligne2hab,
                 'adresse_habitation[ligne3]': ligne3hab,
                 'adresse_habitation[ville]': villehab,
                 'adresse_habitation[npa]': NoNPAhab,
 
-                'adresseDeDepart[ligne1]': ligne1loc,
-                'adresseDeDepart[ligne2]': ligne2loc,
-                'adresseDeDepart[ligne3]': ligne3loc,
-                'adresseDeDepart[ville]': villeloc,
-                'adresseDeDepart[npa]': NoNPAloc,
-                'adresseDeFacturation[ligne1]': ligne1fac,
-                'adresseDeFacturation[ligne2]': ligne2fac,
-                'adresseDeFacturation[ligne3]': ligne3fac,
-                'adresseDeFacturation[ville]': villefac,
-                'adresseDeFacturation[npa]': NoNPAfac,
+                'adresse_depart[ligne1]': ligne1loc,
+                'adresse_depart[ligne2]': ligne2loc,
+                'adresse_depart[ligne3]': ligne3loc,
+                'adresse_depart[ville]': villeloc,
+                'adresse_depart[npa]': NoNPAloc,
+                'adresse_facturation[ligne1]': ligne1fac,
+                'adresse_facturation[ligne2]': ligne2fac,
+                'adresse_facturation[ligne3]': ligne3fac,
+                'adresse_facturation[ville]': villefac,
+                'adresse_facturation[npa]': NoNPAfac,
                 NoAVS: avs,
                 BanqueNom: banquenom,
                 BanqueBIC: banquebic,
                 BanqueIBAN: banqueiban,
+                LimiteTempsTransport: limitetemps,
                 cv: fileCV
-                },
+                },*/
             success: function (data) {
                 console.log("Successfully connected!");
                 console.log(JSON.stringify(data));
