0108e5d4ed967b907263f7abbde034f1b6bb5fd5
OakAndAspen <irinadespot@gmail.com>
2018-06-14 14:33:28 +0200

    2018.06.14 14:33 Divers corrections pour démo 2

--- src/collections/Requetes.js
+++ src/collections/Requetes.js
@@ -1,8 +1,16 @@
 import Backbone from "backbone";
 import AppConfig from "config";
 import Requete from "models/Requete";
+import Requetes from "collections/Requetes";
 
 export default Backbone.Collection.extend({
     model: Requete,
-    url: AppConfig.apiUrl + "/requetes"
+    url: AppConfig.apiUrl + "/requetes",
+
+    sansAcceptees: function () {
+        let filtered = this.filter(function (model) {
+            return model.get("statut") != 'accepte';
+        });
+        return new Requetes(filtered);
+    }
 });
